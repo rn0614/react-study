bd8cedf24088862ea5d63bfa8f7e38c5ab459bd8
rn0614 <rn0614@naver.com>
2022-07-12 22:36:31 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './style.css';
 import { useState } from 'react';
 
 function Header(props) {
-  //console.log('props', props, props.title);
+  console.log('props', props, props.title);
   return (
     <header>
       <h1>
@@ -83,6 +83,48 @@ function Create(props) {
     </article>
   );
 }
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return (
+    <article>
+      <h2>Update</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onUpdate(title, body);
+        }}
+      >
+        <p>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <textarea
+            name="body"
+            cols="30"
+            rows="10"
+            value={body}
+            onChange={(event) => {
+              setBody(event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <input type="submit" value="Update" />
+        </p>
+      </form>
+    </article>
+  );
+}
 // 초기 실행 한번시 실행되는 문장
 export default function App() {
   const [mode, setMode] = useState('WELCOME');
@@ -108,17 +150,35 @@ export default function App() {
     }
     content = <Article title={title} body={body}></Article>;
     contextControl = (
-      <li>
-        <a
-          href="/update"
-          onClick={(event) => {
-            event.preventDefault();
-            setMode('UPDATE');
-          }}
-        >
-          update
-        </a>
-      </li>
+      <>
+        <li>
+          <a
+            href={'/update/' + id}
+            onClick={(event) => {
+              event.preventDefault();
+              setMode('UPDATE');
+            }}
+          >
+            update
+          </a>
+        </li>
+        <li>
+          <input
+            type="button"
+            value="Delete"
+            onClick={() => {
+              const newTopics = [];
+              for (let i = 0; i < topics.length; i++) {
+                if (topics[i].id !== id) {
+                  newTopics.push(topics[i]);
+                }
+              }
+              setTopics(newTopics);
+              setMode("WELCOME");
+            }}
+          />
+        </li>
+      </>
     );
   } else if (mode === 'CREATE') {
     content = (
@@ -133,6 +193,33 @@ export default function App() {
           setNextId(nextId + 1);
         }}
       ></Create>
+    );
+  } else if (mode === 'UPDATE') {
+    let title,
+      body = null;
+    for (let topic of topics) {
+      if (topic.id === id) {
+        title = topic.title;
+        body = topic.body;
+      }
+    }
+    content = (
+      <Update
+        title={title}
+        body={body}
+        onUpdate={(title, body) => {
+          console.log(title, body);
+          const newTopics = [...topics];
+          const updatedTopic = { id: id, title: title, body: body };
+          for (let i = 0; i < topics.length; i++) {
+            if (newTopics[i].id == updatedTopic.id) {
+              newTopics[i] = updatedTopic;
+              break;
+            }
+          }
+          setTopics(newTopics);
+        }}
+      ></Update>
     );
   }
   return (
